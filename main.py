34ae08a9d46a37610ec8805685533462ffec76ed
yobibyte <vitaliykurin@gmail.com>
2021-01-30 00:29:48 +0000

    Add 2021 support.

--- main.py
+++ main.py
@@ -7,52 +7,33 @@ import time
 
 # TODO go back
 
-CONFERENCE_URL = "https://openreview.net/group?id=ICLR.cc/2021/Conference"
-METADATA_FILE = "iclr19_metadata.json"
+YEAR = 2021
+METADATA_FILE = f"iclr{YEAR}_metadata.json"
 
 
-def download_iclr19(client, outdir="./"):
-    # this function was taken from here: https://openreview-py.readthedocs.io/en/latest/getting_data.html#retrieving-all-official-reviews-for-a-conference
+def download_iclr(client):
+    # this function is a modified version of:
+    # https://openreview-py.readthedocs.io/en/latest/getting_data.html
 
     print("getting metadata...")
-    # get all ICLR '19 submissions, reviews, and meta reviews, and organize them by forum ID
-    # (a unique identifier for each paper; as in "discussion forum").
     submissions = openreview.tools.iterget_notes(
-        client, invitation="ICLR.cc/2019/Conference/-/Blind_Submission"
+        client, invitation=f"ICLR.cc/{YEAR}/Conference/-/Blind_Submission"
     )
+
     submissions_by_forum = {n.forum: n for n in submissions}
-
-    # There should be 3 reviews per forum.
-    reviews = openreview.tools.iterget_notes(
-        client, invitation="ICLR.cc/2019/Conference/-/Paper.*/Official_Review"
-    )
-    reviews_by_forum = defaultdict(list)
-    for review in reviews:
-        reviews_by_forum[review.forum].append(review)
-
-    # Because of the way the Program Chairs chose to run ICLR '19, there are no "decision notes";
-    # instead, decisions are taken directly from Meta Reviews.
     meta_reviews = openreview.tools.iterget_notes(
-        client, invitation="ICLR.cc/2019/Conference/-/Paper.*/Meta_Review"
+        client, invitation=f"ICLR.cc/{YEAR}/Conference/Paper.*/-/Decision"
     )
     meta_reviews_by_forum = {n.forum: n for n in meta_reviews}
-
-    # Build a list of metadata.
-    # For every paper (forum), get the review ratings, the decision, and the paper's content.
     metadata = []
-    for forum in submissions_by_forum:
-
-        forum_reviews = reviews_by_forum[forum]
-        review_ratings = [n.content["rating"] for n in forum_reviews]
-
+    t1 = set(submissions_by_forum.keys())
+    t2 = set(meta_reviews_by_forum.keys())
+    for forum in t1.intersection(t2):
         forum_meta_review = meta_reviews_by_forum[forum]
-        decision = forum_meta_review.content["recommendation"]
-
+        decision = forum_meta_review.content["decision"]
         submission_content = submissions_by_forum[forum].content
-
         forum_metadata = {
             "forum": forum,
-            "review_ratings": review_ratings,
             "decision": decision,
             "submission_content": submission_content,
         }
@@ -60,7 +41,7 @@ def download_iclr19(client, outdir="./"):
 
     print("writing metadata to file...")
     # write the metadata, one JSON per line:
-    with open(os.path.join(outdir, METADATA_FILE), "w") as file_handle:
+    with open(os.path.join(".", METADATA_FILE), "w") as file_handle:
         for forum_metadata in metadata:
             file_handle.write(json.dumps(forum_metadata) + "\n")
 
@@ -73,7 +54,7 @@ READING_LIST_FNAME = "reading_list.csv"
 
 def main(win):
     data = []
-    with open("iclr19_metadata.json", "r") as json_file:
+    with open(f"iclr{YEAR}_metadata.json", "r") as json_file:
         for i, line in enumerate(json_file):
             paper = json.loads(line)
             if "Accept" in paper["decision"]:
@@ -98,21 +79,24 @@ def main(win):
         if done:
             break
         paper = data[i]
-
         win.addstr(f"-----> PROGRESS {i}/{n_papers} <-----\n")
         win.addstr(f"TITLE: {paper['submission_content']['title']}\n")
+        win.refresh()
         win.addstr(f"AUTHORS: {paper['submission_content']['authors']}\n")
         try:
-            win.addstr(f"TL;DR: {paper['submission_content']['TL;DR']}\n")
+            win.addstr(
+                f"TL;DR: {paper['submission_content']['one-sentence_summary']}\n"
+            )
         except:
             win.addstr("TL;DR:\n")
         win.addstr(f"DECISION: {paper['decision']}\n")
-        win.addstr(f"ABSTRACT: {paper['submission_content']['abstract']}\n")
+        win.addstr(f"ABSTRACT: {paper['submission_content']['abstract']}\n\n")
 
         win.addstr(
             "[Right arrow key] to add. [Left arrow key] to skip. [q] to quit (your progress will be saved).\n"
         )
         win.refresh()
+
         while True:
             key = win.getch()
             if key == RIGHT_ARROW:
@@ -144,5 +128,5 @@ def main(win):
 if __name__ == "__main__":
     if not os.path.exists(METADATA_FILE):
         guest_client = openreview.Client(baseurl="https://api.openreview.net")
-        download_iclr19(guest_client, ".")
+        download_iclr(guest_client)
     curses.wrapper(main)
